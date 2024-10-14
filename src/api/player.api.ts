cefd1e47cb980955f163a5d4fab94607e903dfde
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-10-14 20:56:27 +0900

    Feat: 선수 스탯 불러오기 API 추가

--- src/api/player.api.ts
+++ src/api/player.api.ts
@@ -1,9 +1,23 @@
-import { Player } from "@/models/player.models";
+import { Player, PlayerGameStats, PlayerSeasonStats } from "@/models/player.models";
 import { httpClient } from "./http";
 
 
 export const getPlayersFromTeam = async (teamId: string | number) => {
-  console.log("teamId", teamId);
   const response = await httpClient.get<Player[]>(`/api/teams/${teamId}/players/`);
   return response.data as Player[];
+}
+
+export const getPlayerSeasonStats = async (teamId: string | number, playerId: string | number) => {
+  const response = await httpClient.get<PlayerSeasonStats>(`/api/teams/${teamId}/players/${playerId}/season-stats/`);
+  return response.data;
+}
+
+export const getPlayerCareerStats = async (teamId: string | number, playerId: string | number) => {
+  const response = await httpClient.get<PlayerSeasonStats[]>(`/api/teams/${teamId}/players/${playerId}/career-stats/`);
+  return response.data;
+}
+
+export const getPlayerLast5GamesStats = async (teamId: string | number, playerId: string | number) => {
+  const response = await httpClient.get<PlayerGameStats[]>(`/api/teams/${teamId}/players/${playerId}/last-5-games/`);
+  return response.data;
 }
