737112ece6d0fdd7adb688d288de686c211ef27b
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-11-05 01:25:05 +0900

    Feat: 마이 페이지 관련 API 추가

--- src/api/team.api.ts
+++ src/api/team.api.ts
@@ -2,53 +2,63 @@ import {
   ConferenceStandings, 
   Team, 
   TeamFranchiseHistory, 
+  TeamWithLikes,
   TeamPost, 
   TeamPostComment, 
   TeamPostCommentLikes, 
-  TeamPostCommentReply, 
   TeamPostCommentReplyPaginationResult, 
   TeamPostCommentsPaginationResult, 
   TeamPostPaginationResult, 
-  TeamPostStatus 
+  TeamPostStatus, 
+  TTeamLikesResult
 } from "@/models/team.models";
 import { httpClient } from "./http";
 import { Game } from "@/models/game.models";
-import { teardownHeapProfiler } from "next/dist/build/swc";
 
 
 export const getAllTeams = async () => {
-    const response = await httpClient.get<Team[]>(`/api/teams/`);
-    return response.data as Team[];
+  const response = await httpClient.get<Team[]>(`/api/teams/`);
+  return response.data as Team[];
 }
 
 export const getUserFavoriteTeams = async () => {
-    const response = await httpClient.get<Team[]>(`/api/users/me/favorite-teams/`);
-    return response.data as Team[];
+  const response = await httpClient.get<Team[]>(`/api/users/me/favorite-teams/`);
+  return response.data as Team[];
 }
 
 export const updateUserFavoriteTeams = async (teams: Team[]) => {
-    const teamIds : {id: number}[] = [];
-    for (let i = 0; i < teams.length; i++) {
-        teamIds.push({id: teams[i].id});
-    };
+  const teamIds : {id: number}[] = [];
+  for (let i = 0; i < teams.length; i++) {
+    teamIds.push({id: teams[i].id});
+  };
 
-    const response = await httpClient.put<typeof teamIds>(`/api/users/me/favorite-teams/`, teamIds);
-    return response.data;
+  const response = await httpClient.put<typeof teamIds>(`/api/users/me/favorite-teams/`, teamIds);
+  return response.data;
+}
+
+export const addUserFavoriteTeam = async (teamId: string) => {
+  const response = await httpClient.post<TTeamLikesResult>(`/api/users/me/favorite-teams/${teamId}/`);
+  return response.data as TTeamLikesResult;
+}
+
+export const removeUserFavoriteTeam = async (teamId: string) => {
+  const response = await httpClient.delete<TTeamLikesResult>(`/api/users/me/favorite-teams/${teamId}/`);
+  return response.data as TTeamLikesResult;
 }
 
 export const getTeamGeneralInfo = async (teamId: string) => {
-    const response = await httpClient.get<Team>(`/api/teams/${teamId}/`);
-    return response.data as Team;
+  const response = await httpClient.get<TeamWithLikes>(`/api/teams/${teamId}/`);
+  return response.data as TeamWithLikes;
 }
 
 export const getTeamFranchiseHistory = async (teamId: string) => {
-    const response = await httpClient.get<TeamFranchiseHistory>(`/api/teams/${teamId}/franchise-history/`);
-    return response.data as TeamFranchiseHistory;
+  const response = await httpClient.get<TeamFranchiseHistory>(`/api/teams/${teamId}/franchise-history/`);
+  return response.data as TeamFranchiseHistory;
 }
 
 export const getTeamPosts = async (teamId: string, page: number) => {
-    const response = await httpClient.get<TeamPostPaginationResult>(`/api/teams/${teamId}/posts/?page=${page}`);
-    return response.data as TeamPostPaginationResult;
+  const response = await httpClient.get<TeamPostPaginationResult>(`/api/teams/${teamId}/posts/?page=${page}`);
+  return response.data as TeamPostPaginationResult;
 }
 
 export const getTeamPostStatus = async () => {
@@ -62,13 +72,13 @@ export const getTeamPostStatusForCreate = async () => {
 }
 
 export const getTeamsStandings = async () => {
-    const response = await httpClient.get<ConferenceStandings>(`/api/teams/standings/`);
-    return response.data as ConferenceStandings;
+  const response = await httpClient.get<ConferenceStandings>(`/api/teams/standings/`);
+  return response.data as ConferenceStandings;
 }
 
 export const getLast4Games = async (teamId: string) => {
-    const response = await httpClient.get<Game[]>(`/api/teams/${teamId}/last-4-games/`);
-    return response.data as Game[];
+  const response = await httpClient.get<Game[]>(`/api/teams/${teamId}/last-4-games/`);
+  return response.data as Game[];
 }
 
 export const publishTeamPost = async (
@@ -83,6 +93,16 @@ export const publishTeamPost = async (
 
 export const getTeamPost = async (teamId: string, postId: string) => {
   const response = await httpClient.get<TeamPost>(`/api/teams/${teamId}/posts/${postId}/`);
+  return response.data;
+}
+
+export const addTeamPostLike = async (teamId: string, postId: string) => {
+  const response = await httpClient.post<TeamPostCommentLikes>(`/api/teams/${teamId}/posts/${postId}/likes/`);
+  return response.data;
+}
+
+export const removeTeamPostLike = async (teamId: string, postId: string) => {
+  const response = await httpClient.delete<TeamPostCommentLikes>(`/api/teams/${teamId}/posts/${postId}/likes/`);
   return response.data;
 }
 
@@ -102,6 +122,25 @@ export const publishTeamPostComment = async (
   content: string
 ) => {
   const response = await httpClient.post(`/api/teams/${teamId}/posts/${postId}/comments/`, { content });
+  return response.data;
+}
+
+export const editTeamPostComment = async (
+  teamId: string,
+  postId: string,
+  commentId: string,
+  content: string
+) => {
+  const response = await httpClient.put(`/api/teams/${teamId}/posts/${postId}/comments/${commentId}/`, { content });
+  return response.data;
+}
+
+export const deleteTeamPostComment = async (
+  teamId: string,
+  postId: string,
+  commentId: string
+) => {
+  const response = await httpClient.delete(`/api/teams/${teamId}/posts/${postId}/comments/${commentId}/`);
   return response.data;
 }
 
--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,10 +1,21 @@
-import { IUpdateProfileVisibility, IUpdateUserIntroduction, IUser } from "@/models/user.models";
+import { IUpdateProfileVisibility, IUpdateUserIntroduction, IUser, MyPageCommentsPaginationResult } from "@/models/user.models";
 import { httpClient } from "./http";
+import { TeamPostPaginationResult } from "@/models/team.models";
 
 
 export const getMyInfo = async () => {
   const response = await httpClient.get<IUser>("/api/users/me/");
   return response.data;
+}
+
+export const getMyPosts = async (page: number) => {
+  const response = await httpClient.get<TeamPostPaginationResult>(`/api/users/me/posts/?page=${page}`);
+  return response.data as TeamPostPaginationResult;
+}
+
+export const getMyComments = async (page: number) => {
+  const response = await httpClient.get<MyPageCommentsPaginationResult>(`/api/users/me/comments/?page=${page}`);
+  return response.data as MyPageCommentsPaginationResult;
 }
 
 export const getUserInfo = async (userId: number) => {
