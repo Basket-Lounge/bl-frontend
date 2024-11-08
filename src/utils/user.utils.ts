041d8e8932acad4af28bb0d7174a3dc1269c1e7c
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-11-09 02:18:42 +0900

    Feat: 유저 관련 유틸 함수 추가

--- src/utils/user.utils.ts
+++ src/utils/user.utils.ts
@@ -1,4 +1,4 @@
-import { UserChatMessage, UserChatMessageWithUserData } from "@/models/user.models";
+import { UserChat, UserChatMessage, UserChatMessageWithUserData, UserChatParticipants } from "@/models/user.models";
 
 export const translateRoleNameToKorean = (role: string) => {
   switch (role) {
@@ -19,9 +19,53 @@ export const translateRoleNameToKorean = (role: string) => {
   }
 }
 
+export const createChannelNameForPrivateChat = (chatId: string, participants: UserChatParticipants[]) => {
+  const participants1Id = participants[0].user_data.id;
+  const participants2Id = participants[1].user_data.id;
+
+  return chatId + `#${participants1Id},${participants2Id}`;
+}
+
 
 export const sortUserChatMessagesByDate = (messages: UserChatMessageWithUserData[]) => {
   return messages.sort((a, b) => {
     return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
   });
+}
+
+export const sortUserChatByLastMessageDate = (
+  chats: UserChat[], 
+  newChat: UserChat, 
+) => {
+  const newChatIndex = chats.findIndex((chat) => chat.id === newChat.id);
+  if (newChatIndex === -1) {
+    chats.push(newChat);
+  } else {
+    chats[newChatIndex] = newChat;
+  }
+
+  const newChats = chats.sort((a, b) => {
+    const aLastMessageDate = new Date(a.updated_at).getTime();
+    const bLastMessageDate = new Date(b.updated_at).getTime();
+
+    return bLastMessageDate - aLastMessageDate;
+  });
+
+  while (newChats.length > 10) {
+    newChats.pop();
+  }
+
+  return newChats;
+}
+
+export const getLastMessageFromUserChat = (chat: UserChat, username: string) => {
+  const user = chat.participants.find(participant => participant.user_data.username === username);
+  const otherUser = chat.participants.find(participant => participant.user_data.username !== username);
+
+  const userlastMessage = user?.last_message?.created_at || '2000-01-01T00:00:00Z';
+  const otherUserlastMessage = otherUser?.last_message?.created_at || '2000-01-01T00:00:00Z';
+
+  return new Date(userlastMessage).getTime() > new Date(otherUserlastMessage).getTime() ? 
+    user?.last_message : 
+    otherUser?.last_message;
 }
