ec3946ffcf55d0c1baff5c48c78888c0c39b2f0e
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-11-11 23:54:01 +0900

    Feat: 유저 문의 관련 유틸 추가
    - 문의 생성 관련 유효성 검사 함수

--- src/utils/user.utils.ts
+++ src/utils/user.utils.ts
@@ -1,4 +1,5 @@
-import { UserChat, UserChatMessage, UserChatMessageWithUserData, UserChatParticipants } from "@/models/user.models";
+import { InquiryType, UserChat, UserChatMessage, UserChatMessageWithUserData, UserChatParticipants, UserInquiry, UserInquiryModerator } from "@/models/user.models";
+
 
 export const translateRoleNameToKorean = (role: string) => {
   switch (role) {
@@ -25,7 +26,6 @@ export const createChannelNameForPrivateChat = (chatId: string, participants: Us
 
   return chatId + `#${participants1Id},${participants2Id}`;
 }
-
 
 export const sortUserChatMessagesByDate = (messages: UserChatMessageWithUserData[]) => {
   return messages.sort((a, b) => {
@@ -58,6 +58,37 @@ export const sortUserChatByLastMessageDate = (
   return newChats;
 }
 
+export const sortUserInquiryMessagesByDate = (messages: UserChatMessage[]) => {
+  return messages.sort((a, b) => {
+    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+  });
+}
+
+export const sortUserInquiriesByLastMessageDate = (
+  inquiries: UserInquiry[],
+  newInquiry: UserInquiry,
+) => {
+  const newInquiryIndex = inquiries.findIndex((inquiry) => inquiry.id === newInquiry.id);
+  if (newInquiryIndex === -1) {
+    inquiries.push(newInquiry);
+  } else {
+    inquiries[newInquiryIndex] = newInquiry;
+  }
+
+  const newInquiries = inquiries.sort((a, b) => {
+    const aLastMessageDate = new Date(a.updated_at).getTime();
+    const bLastMessageDate = new Date(b.updated_at).getTime();
+
+    return bLastMessageDate - aLastMessageDate;
+  });
+
+  while (newInquiries.length > 10) {
+    newInquiries.pop();
+  }
+
+  return newInquiries;
+}
+
 export const getLastMessageFromUserChat = (chat: UserChat, username: string) => {
   const user = chat.participants.find(participant => participant.user_data.username === username);
   const otherUser = chat.participants.find(participant => participant.user_data.username !== username);
@@ -68,4 +99,33 @@ export const getLastMessageFromUserChat = (chat: UserChat, username: string) => 
   return new Date(userlastMessage).getTime() > new Date(otherUserlastMessage).getTime() ? 
     user?.last_message : 
     otherUser?.last_message;
+}
+
+export const getLastMessageFromUserInquiry = (inquiry: UserInquiry) => {
+  let recentModeratorMessage: UserChatMessage | undefined;
+  inquiry.moderators.forEach((moderator: UserInquiryModerator) => {
+    if (!recentModeratorMessage) {
+      recentModeratorMessage = moderator.last_message;
+    } else {
+      const oldDate = new Date(recentModeratorMessage.created_at).getTime();
+      const newDate = new Date(moderator.last_message?.created_at || '2000-01-01T00:00:00Z').getTime();
+      if (newDate > oldDate) {
+        recentModeratorMessage = moderator.last_message;
+      }
+    }
+  });
+
+  const userlastMessage = inquiry.last_message?.created_at || '2000-01-01T00:00:00Z';
+
+  return new Date(userlastMessage).getTime() > new Date(recentModeratorMessage?.created_at || '2000-01-01T00:00:00Z').getTime() ? 
+    inquiry.last_message : 
+    recentModeratorMessage;
+}
+
+export const extractInquiryTypeNameInEnglish = (inquiryType: InquiryType) => {
+  return inquiryType.display_names.find(name => name.language_data.name === "English")?.display_name || "";
+}
+
+export const extractInquiryTypeNameInKorean = (inquiryType: InquiryType) => {
+  return inquiryType.display_names.find(name => name.language_data.name === "Korean")?.display_name || "";
 }
--- src/utils/validation.utils.ts
+++ src/utils/validation.utils.ts
@@ -27,3 +27,25 @@ export const TeamPostCommentValidation = z.object({
     message: '최대 2048자까지 입력 가능합니다.'
   }),
 });
+
+export const UserInquiryValidation = z.object({
+  title: z.string({
+    required_error: '제목을 입력해주세요.',
+  }).min(5, {
+    message: '최소 5자 이상 입력해주세요.'
+  }).max(128, {
+    message: '최대 128자까지 입력 가능합니다.'
+  }),
+
+  typeId: z.number({
+    required_error: '문의 유형을 선택해주세요.',
+  }),
+
+  content: z.string({
+    required_error: '내용을 입력해주세요.',
+  }).min(1,{
+    message: '최소 1자 이상 입력해주세요.'
+  }).max(2048, {
+    message: '최대 2048자까지 입력 가능합니다.'
+  }),
+});
