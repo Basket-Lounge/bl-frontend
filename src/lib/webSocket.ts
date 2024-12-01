e753d5b825bb724fd05c80c81baed1ba2c67788b
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-12-02 04:32:59 +0900

    Modify: 웹소켓 파일 수정

--- src/lib/webSocket.ts
+++ src/lib/webSocket.ts
@@ -1,4 +1,4 @@
 import { Centrifuge } from "centrifuge";
 
 
-export const client = new Centrifuge("ws://127.0.0.1:8000/connection/websocket");
+export const client = new Centrifuge(`${process.env.NEXT_PUBLIC_CENTRIFUGO_SERVER_WS_URL}/connection/websocket`);
