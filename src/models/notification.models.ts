ab9a274b204055197176aefd2fad44796a9d0656
Ud293nBqda@outlook.kr <johndoe@example.com>
2025-01-06 18:19:38 +0900

    Feat: Various model interfaces and stores for Notification

---
+++ src/models/notification.models.ts
@@ -0,0 +1,26 @@
+export interface INotification {
+  id: number;
+  picture_url: string | null;
+  redirect_url: string | null;
+  contents: INotificationContents;
+  created_at: string;
+  updated_at: string;
+  recipients: INotificationRecipient[];
+}
+
+export interface INotificationPaginationResult {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: INotification[];
+}
+
+export interface INotificationRecipient {
+  recipient_data: {id: number, username: string};
+  read: boolean;
+  read_at: string | null;
+}
+
+export interface INotificationContents {
+  [key: string]: string;
+}
